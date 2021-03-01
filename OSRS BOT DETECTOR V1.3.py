04dce7a8ee88d5dc00a2aa3d3e98ac5f55575879
Ferrariic <thearcticfalcon@gmail.com>
2021-03-01 12:10:44 -0500

    Update OSRS BOT DETECTOR V1.3.py

--- OSRS BOT DETECTOR V1.3.py
+++ OSRS BOT DETECTOR V1.3.py
@@ -1,17 +1,18 @@
+#######################################################################################
 # BSD 2-Clause License
 
 # Copyright (c) 2021, Ferrariic
-#All rights reserved.
+# All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
-#modification, are permitted provided that the following conditions are met:
+# modification, are permitted provided that the following conditions are met:
 
-# 1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
+# 1. Redistributions of source code must retain the above copyright notice, 
+    # this list of conditions and the following disclaimer.
 
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
+ #2. Redistributions in binary form must reproduce the above copyright notice, 
+    # this list of conditions and the following disclaimer in the documentation 
+    # and/or other materials provided with the distribution.
 
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -23,6 +24,7 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#######################################################################################
 
 import pickle
 import time
