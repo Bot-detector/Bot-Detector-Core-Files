a8fa18f09f2690a100f78733d260a4a95657ea66
Ferrariic <thearcticfalcon@gmail.com>
2021-03-01 12:10:24 -0500

    Update OSRS BOT HISCORE NAME PULLER V1.2.py

--- OSRS BOT HISCORE NAME PULLER V1.2.py
+++ OSRS BOT HISCORE NAME PULLER V1.2.py
@@ -1,10 +1,30 @@
-#!/usr/bin/env python
-# coding: utf-8
+#######################################################################################
+# BSD 2-Clause License
 
-# In[6]:
+# Copyright (c) 2021, Ferrariic
+# All rights reserved.
 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 
-#Author RSN: FERRARIIC 2/7/2021
+# 1. Redistributions of source code must retain the above copyright notice, 
+    # this list of conditions and the following disclaimer.
+
+ #2. Redistributions in binary form must reproduce the above copyright notice, 
+    # this list of conditions and the following disclaimer in the documentation 
+    # and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#######################################################################################
 
 import numpy as np
 import re
@@ -17,10 +37,9 @@ import urllib
 from urllib import request
 import requests
 from IPython.display import clear_output
-########################################################################################
 
-MAIN = "http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" #main source url
-DATADIR = "C:\Datasets\OSRSPlayerlist\BLAST.txt" #Filename for player names [#####MODIFY THIS VARIABLE POSSIBLE#####]
+MAIN = "http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" 
+DATADIR = "C:\Datasets\OSRSPlayerlist\BLAST.txt" 
 
 fRAW = open(DATADIR, "rt") #opens raw bot file
 fCLEAN = open("2162021BLAST.txt", "wt") #starts new bot file type
