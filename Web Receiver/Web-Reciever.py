099ff3d6bd5e04832e2321f387875227672fc9e4
Ferrariic <thearcticfalcon@gmail.com>
2021-03-04 17:27:25 -0500

    added an IP checker and updated parser to latest version

--- Web Receiver/Web-Reciever.py
+++ Web Receiver/Web-Reciever.py
@@ -26,33 +26,72 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #######################################################################################
 
+from flask import request, abort, current_app as app
 from flask import Flask
 from flask import request
 import re
+import time
+
+line_seen = set()
+response = 0
+ip_timelog = []
+ip_ban_list = []
+check = 0
+ipr = []
 
 app = Flask(__name__)
 
-line_seen = set()
+@app.before_request
+def block_method(r = 0):
+    ip = request.environ.get('REMOTE_ADDR')
+    
+    if ip in ipr:
+        r = ipr.index(ip)
+        check = 1
+    else:
+        ip_timelog.append([ip,time.time()])
+        check = 0
+        ipr.append(ip)
+    
+    last_time = ip_timelog[r][1]
+    response = time.time()-last_time
+    ip_timelog[r][1] = time.time()
+    
+    if check == 1:
+        if response < 3:
+            if ip in ip_ban_list:
+                abort(403)
+            else:
+                ip_ban_list.append(ip)
+                abort(403)
+        else:
+            if ip in ip_ban_list:
+                abort(403)
+    print("Response Time: ", response)
+    print("Timelog: ", ip_timelog)
+    print("IP REC: ", ipr)
+    print("Banlist: ", ip_ban_list)
 
 @app.route('/', methods =['POST'])
 def post():
         if request.method == 'POST':
             print(request.data)
             datastr = str(request.data)
-            dataclean = datastr[1:].replace('\\r', '').strip("'").replace('\\n','\n')
+            dataclean = datastr[1:].replace('\\r', '').strip("'[]").replace(', ','\n')
             with open("TempINPUT.txt", "wt") as SERVERDATARAW:
                 SERVERDATARAW.write(dataclean)
             SERVERDATARAW.close()
             SERVERDATARAW = open("TempINPUT.txt", "rt")
-            with open("PLAYERGATHERDATA1.txt", "a") as SERVERDATA:
+            with open("PLAYERGATHERDATA2.txt", "a") as SERVERDATA:
                 for line in SERVERDATARAW:
                     if len(line)<13:
                         L = re.findall('[a-zA-Z0-9_-] *', line)
                         line = ''.join(map(str, L))
                         if line not in line_seen:
                             SERVERDATA.write(line + '\n')
-                            line_seen.add(line)   
+                            line_seen.add(line)
+            
             return 'FINISHED'
             
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port="8000")
+    app.run(port="8000")
