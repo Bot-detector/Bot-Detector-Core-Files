042793db0ff19b5460b7af01202de72c31042be8
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-03-16 23:27:20 -0400

    Fixed JSON formatting on the return string. Again.

--- mysite/dashboard.py
+++ mysite/dashboard.py
@@ -12,6 +12,6 @@ dashboard = Blueprint('dashboard', __name__, template_folder='templates')
 @dashboard.route('/site/dashboard/gettotalbans', methods=['GET'])
 def get_total_bans():
     num_of_bands = get_number_confirmed_bans()
-    return_str = "{\"bans\": " + str(num_of_bands) + "}"
+    return_str = '{"bans": " + str(num_of_bands) + "}'
 
     return jsonify(return_str)
