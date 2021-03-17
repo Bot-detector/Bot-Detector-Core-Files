dbe74d4bf240688fb29c51fbc5d3ef1379aa564e
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-03-16 23:41:32 -0400

    Resolving more JSON formatting errors.

--- mysite/dashboard.py
+++ mysite/dashboard.py
@@ -7,24 +7,11 @@ dashboard = Blueprint('dashboard', __name__, template_folder='templates')
 
 #######################
 # Dashboard Endpoints #
-#                     #
-#                     #
-#                     #
 #######################
 
 @dashboard.route('/site/dashboard/gettotalbans', methods=['GET'])
 def get_total_bans():
     num_of_bands = get_number_confirmed_bans()
-    return_str = '{"bans": " + str(num_of_bands) + "}'
+    return_str = '{"bans": "' + str(num_of_bands) + '"}'
 
     return jsonify(return_str)
-
-
-
-
-#CORS Policy: Allow Access to These Methods From Any Origin
-@dashboard.after_request
-def after_request(response):
-    header = response.headers
-    header['Access-Control-Allow-Origin'] = '*'
-    return response
