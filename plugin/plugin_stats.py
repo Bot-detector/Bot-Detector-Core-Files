d1b301417c3b1c1cefe4ee58171b80ca7026da2b
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-03-18 14:20:10 -0400

    Added route for get_contributions at /stats/contributions/<contributor>

--- plugin/plugin_stats.py
+++ plugin/plugin_stats.py
@@ -7,13 +7,12 @@ plugin_stats = Blueprint('plugin_stats', __name__, template_folder='templates')
 
 @plugin_stats.route('/stats/contributions/<contributor>', methods=['GET'])
 def get_contributions(contributor=""):
-    contributions = SQL.get_contributions(contributor)
 
-    return_dict = {
-        "num_reported": 0,
-        "players_banned": 0
-    }
+    if(contributor==""):
+        return "<h1>404</h1><p>You must include a Runescape Name in your query.</p>", 400
+    else:
+        contributions = SQL.get_contributions(contributor)
 
-    return "OK"
+        return contributions
 
 
