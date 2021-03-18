006875380d939d2565955ec13e0e4302d66b61df
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-03-17 20:50:16 -0400

    Removed get_total_bans as it was redundant.

--- mysite/dashboard.py
+++ mysite/dashboard.py
@@ -9,15 +9,6 @@ dashboard = Blueprint('dashboard', __name__, template_folder='templates')
 # Dashboard Endpoints #
 #######################
 
-@dashboard.route('/site/dashboard/gettotalbans', methods=['GET'])
-def get_total_bans():
-    num_of_bans = get_number_confirmed_bans()
-    return_dict = {
-        "bans": num_of_bans
-    }
-
-    return jsonify(return_dict)
-
 @dashboard.route('/site/dashboard/gettotaltrackedplayers', methods=['GET'])
 def get_total_tracked_players():
     num_of_players = get_number_tracked_players()
