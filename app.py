ff2b945e8e3cacdef8efcc712ad6e21f3832130d
y c <yentl.bilie@hotmail.com>
2021-03-26 01:25:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ app.register_blueprint(dashboard)
 
 if not app.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
     started = True
-    sched.add_job(run_hiscore, 'interval', minutes=10, start_date=datetime.date.today(), replace_existing=False, max_instances=10, name='run_hiscore')
+    sched.add_job(run_hiscore, 'interval', minutes=10, start_date=datetime.date.today(), replace_existing=False, name='run_hiscore')
     sleep(1)
     sched.start()
 
