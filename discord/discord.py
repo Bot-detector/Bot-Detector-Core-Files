f59cd14a45d396d7b0144b8048d52ca0fff82e55
extreme4all <yentl.bilie@gmail.com>
2021-04-20 15:06:48 +0200

    basic layout

---
+++ discord/discord.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from flask.json import jsonify
+import SQL
+
+discord = Blueprint('discord', __name__, template_folder='templates')
+
+
+@discord.route('/discord/locations/<token>/<player_name>', methods=['GET'])
+def get_location():
+    pass
