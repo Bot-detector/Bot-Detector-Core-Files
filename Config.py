e041627304cff88f069be70d48f14bdf4bf724d6
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-05-07 11:03:46 +0200

    devmode doesnt like bool() (#96) (#97)
    Co-authored-by: extreme4all <yentl.bilie@gmail.com>
    
    Co-authored-by: extreme4all <yentl.bilie@gmail.com>

--- Config.py
+++ Config.py
@@ -16,8 +16,9 @@ proxy_http = os.environ.get('proxy_http')
 proxy_https = os.environ.get('proxy_https')
 flask_port = os.environ.get('flask_port')
 # TODO: BUG
+# it does not like the bool()
 try:
-    dev_mode = bool(os.environ.get('dev_mode'))
+    dev_mode = os.environ.get('dev_mode')
 except:
     dev_mode=1
 
