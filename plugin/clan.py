57d9570f7cf7a861672245afca2d3afb338c3c02
y c <40169115+extreme4all@users.noreply.github.com>
2021-05-31 21:51:53 +0200

    Create clan.py

---
+++ plugin/clan.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import SQL
+from flask import Blueprint, request
+
+clan = Blueprint('clan', __name__, template_folder='templates')
+
+
+@clan.route('/<version>/plugin/clan/rank-update/<token>', methods=['POST'])
+def get_clan_rank(version, token):
+    '''
+        receive a dictionary of
+        [{
+            'player':'',
+            'rank':''
+        }, ...
+        ]
+        return the same dictionary with the players that have incorrect rank (the returned rank is the correct rank)
+    '''
+    user_ranks = request.get_json()
+    df = pd.DataFrame(user_ranks)
+
+    #TODO
+    # get KC of players
+
+    # parse KC of players
+
+    # return players with their new rank
+    pass
