32b73a488e9dfe4c1d3fed5aafff7ca2d7c00612
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-06-04 12:29:55 +0200

    removed wintertod & farmbird

--- Predictions/model.py
+++ Predictions/model.py
@@ -95,11 +95,11 @@ def train_model(n_pca):
 
     lbls = [
         'Real_Player', 'Smithing_bot', 'Mining_bot', 
-        'Magic_bot', 'PVM_Ranged_bot', 'Wintertodt_bot', 
+        'Magic_bot', 'PVM_Ranged_bot', 
         'Fletching_bot', 'PVM_Melee_bot', 'Herblore_bot',
         'Thieving_bot','Crafting_bot', 'PVM_Ranged_Magic_bot',
         'Hunter_bot','Runecrafting_bot','Fishing_bot','Agility_bot',
-        'Cooking_bot', 'FarmBird_bot', 'mort_myre_fungus_bot',
+        'Cooking_bot', 'mort_myre_fungus_bot',
         'Woodcutting_bot'
     ]
 
