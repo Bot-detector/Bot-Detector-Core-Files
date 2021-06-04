7c404493042894e17804e651b2cb756a49eb61d1
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-06-04 15:43:38 +0200

    save_model route mle

--- mysite/tokens.py
+++ mysite/tokens.py
@@ -149,7 +149,7 @@ def create_predictions(token):
     if not (verify_token(token, verifcation='create_token')):
         return "<h1>404</h1><p>Invalid token</p>", 404
 
-    n_pca = 30
+    n_pca = 'mle'
     Config.sched.add_job(model.save_model ,args=[n_pca], replace_existing=True, name='save_model')
     return jsonify({'OK': 'OK'})
 
