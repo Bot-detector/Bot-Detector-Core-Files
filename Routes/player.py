13252dba0f31d6e56ab56b5f3ab83e608aa64c82
y c <40169115+extreme4all@users.noreply.github.com>
2021-07-09 23:48:46 +0200

    Update player.py

--- Routes/player.py
+++ Routes/player.py
@@ -1,32 +1,63 @@
 
 
-import sqlalchemy
 from Config import engine
 import models
-from flask_restful import Resource, abort
+from flask_restful import Resource, abort, reqparse
 from flask import jsonify
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from sqlalchemy.orm import sessionmaker
 from Routes.functions import sqlalchemy_result
 
-
-engine.echo = True
 Session = sessionmaker(engine)
 
 # TODO: token verification
+def verify_token(token, access):
+    return True
 # abort(404, message="Insufficient permissions")
+
+# TODO: swagger support
+
+parser = reqparse.RequestParser()
+parser.add_argument('id', type=int, location='args')
+parser.add_argument('name', type=str, location='args')
+parser.add_argument('token', type=str, location='headers')
 
 class Player(Resource):
     def get(self):
         '''
-        select form table
+        select from table
         '''
+        # parse arguments
+        args = parser.parse_args()
+        verified = verify_token(args['token'], 'hiscores')
+        verified_args = []
+        # convert arguments to sql filter
+        filters = []
+        for col in args:
+            # we cannot filter with None
+            if args[col] is None or col == 'token':
+                continue
+
+            if not(verified) and col in verified_args:
+                continue
+            
+            filter = (getattr(models.Player, col) == args[col])
+            filters.append(filter)
+
+        # abort if no valid filter is given
+        if filters == []:
+            return abort(404, message="Insufficient arguments")
+        
+        # create query to execute
         select_sql = select(models.Player).where(
-            models.Player.id <10
+            and_(*filters)
         )
+
+        # create session & execute query
         with Session() as session:
             rows = session.execute(select_sql)
         
+        # process & return result
         rows = sqlalchemy_result(rows)
         return jsonify(rows.rows2dict())
     
