b32971a8dda0770be7af432118ca02753a86b8ce
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-07-30 10:22:35 +0200

    add legacy router

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from Config import app
-from routers import hiscore, player, prediction, report
+from routers import hiscore, player, prediction, report, legacy
 
 app.include_router(hiscore.router)
 app.include_router(player.router)
 app.include_router(prediction.router)
 app.include_router(report.router)
+app.include_router(legacy.router)
 
 
 @app.get("/")
