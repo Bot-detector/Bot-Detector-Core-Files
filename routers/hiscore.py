b57633d899592a54d003b0937b0914886ba20405
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-08-03 11:13:03 +0200

    Update hiscore.py

--- routers/hiscore.py
+++ routers/hiscore.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 from database.functions import execute_sql
+import datetime
 
 router = APIRouter()
 
@@ -21,7 +22,7 @@ async def get(
     label_id: Optional[int] = None,
     row_count: int = 100_000,
     page: int = 1
-):
+    ):
     '''
     select data from database
     '''
@@ -53,21 +54,122 @@ async def get(
     return data
 
 @router.get("/v1/hiscoreLatest", tags=["hiscore"])
-async def get():
+async def get(
+    player_name: Optional[str] = None,
+    player_id: Optional[int] = None,
+    label_id: Optional[int] = None,
+    row_count: int = 100_000,
+    page: int = 1
+    ):
     '''
     select data from database
     '''
-    pass
+    sql = ('''
+        select 
+            pl.name, 
+            phd.*
+        from playerHiscoreDataLatest phd
+        inner join Players pl on (phd.Player_id = pl.id)
+        where 1=1
+    ''')
 
-@router.get("/v1/hiscoreExpGain", tags=["hiscore"])
-async def get():
+    param = {
+        'name': player_name,
+        'id': player_id,
+        'label_id': label_id
+    }
+
+    # build query
+    sql_filter = [f' and {k} = :{k}' for k,v in param.items() if v is not None]
+    has_good_param = True if len(sql_filter) > 0 else False
+    sql = f'{sql} {"".join(sql_filter)}'
+
+    # return exception if no param are given
+    if not (has_good_param):
+        raise HTTPException(status_code=404, detail="No valid parameters given")
+
+    data = execute_sql(sql, param, row_count=row_count, page=page).rows2dict()
+    return data
+
+@router.get("/v1/hiscoreXPChange", tags=["hiscore"])
+async def get(
+    player_name: Optional[str] = None,
+    player_id: Optional[int] = None,
+    label_id: Optional[int] = None,
+    row_count: int = 100_000,
+    page: int = 1
+    ):
     '''
     select data from database
     '''
-    pass
+    sql = ('''
+        select 
+            pl.name, 
+            phd.*
+        from playerHiscoreDataXPChange phd
+        inner join Players pl on (phd.Player_id = pl.id)
+        where 1=1
+    ''')
+
+    param = {
+        'name': player_name,
+        'id': player_id,
+        'label_id': label_id
+    }
+
+    # build query
+    sql_filter = [f' and {k} = :{k}' for k,v in param.items() if v is not None]
+    has_good_param = True if len(sql_filter) > 0 else False
+    sql = f'{sql} {"".join(sql_filter)}'
+
+    # return exception if no param are given
+    if not (has_good_param):
+        raise HTTPException(status_code=404, detail="No valid parameters given")
+
+    data = execute_sql(sql, param, row_count=row_count, page=page).rows2dict()
+    return data
+
+@router.get("/v1/hiscoreXPChangeLatest", tags=["hiscore"])
+async def get(
+    player_name: Optional[str] = None,
+    player_id: Optional[int] = None,
+    label_id: Optional[int] = None,
+    row_count: int = 100_000,
+    page: int = 1
+    ):
+    '''
+    select data from database
+    '''
+    sql = ('''
+        select 
+            pl.name, 
+            phd.*
+        from playerHiscoreDataXPChange phd
+        inner join Players pl on (phd.Player_id = pl.id)
+        where 1=1
+    ''')
+
+    param = {
+        'name': player_name,
+        'id': player_id,
+        'label_id': label_id,
+        'ts_date': datetime.datetime.now().strftime('%Y-%m-%d')
+    }
+
+    # build query
+    sql_filter = [f' and {k} = :{k}' for k,v in param.items() if v is not None]
+    has_good_param = True if len(sql_filter) > 0 else False
+    sql = f'{sql} {"".join(sql_filter)}'
+
+    # return exception if no param are given
+    if not (has_good_param):
+        raise HTTPException(status_code=404, detail="No valid parameters given")
+
+    data = execute_sql(sql, param, row_count=row_count, page=page).rows2dict()
+    return data
 
 @router.post("/v1/hiscore", tags=["hiscore"])
-async def post(hiscores:hiscore):
+async def post(hiscores: hiscore):
     '''
     insert data into database
     '''
