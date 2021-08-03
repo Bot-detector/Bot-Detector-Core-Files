a0e554d7f45f4e265e2812721ff7dfbf6bda74c2
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-08-03 13:26:01 +0200

    Update hiscore.py

--- routers/hiscore.py
+++ routers/hiscore.py
@@ -1,7 +1,8 @@
+import logging
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
-from database.functions import execute_sql
+from database.functions import execute_sql, list_to_string
 import datetime
 
 router = APIRouter()
@@ -13,6 +14,87 @@ class hiscore(BaseModel):
     '''
     player_id: int
     total: int
+    Attack: int
+    Defence: int
+    Strength: int
+    Hitpoints: int
+    Ranged: int
+    Prayer: int
+    Magic: int
+    Cooking: int
+    Woodcutting: int
+    Fletching: int
+    Fishing: int
+    Firemaking: int
+    Crafting: int
+    Smithing: int
+    Mining: int
+    Herblore: int
+    Agility: int
+    Thieving: int
+    Slayer: int
+    Farming: int
+    Runecraft: int
+    Hunter: int
+    Construction: int
+    league: int
+    bounty_hunter_hunter: int
+    bounty_hunter_rogue: int
+    cs_all: int
+    cs_beginner: int
+    cs_easy: int
+    cs_medium: int
+    cs_hard: int
+    cs_elite: int
+    cs_master: int
+    lms_rank: int
+    soul_wars_zeal: int
+    abyssal_sire: int
+    alchemical_hydra: int
+    barrows_chests: int
+    bryophyta: int
+    callisto: int
+    cerberus: int
+    chambers_of_xeric: int
+    chambers_of_xeric_challenge_mode: int
+    chaos_elemental: int
+    chaos_fanatic: int
+    commander_zilyana: int
+    corporeal_beast: int
+    crazy_archaeologist: int
+    dagannoth_prime: int
+    dagannoth_rex: int
+    dagannoth_supreme: int
+    deranged_archaeologist: int
+    general_graardor: int
+    giant_mole: int
+    grotesque_guardians: int
+    hespori: int
+    kalphite_queen: int
+    king_black_dragon: int
+    kraken: int
+    kreearra: int
+    kril_tsutsaroth: int
+    mimic: int
+    nightmare: int
+    obor: int
+    sarachnis: int
+    scorpia: int
+    skotizo: int
+    tempoross:int
+    the_gauntlet: int
+    the_corrupted_gauntlet: int
+    theatre_of_blood: int
+    theatre_of_blood_hard: int
+    thermonuclear_smoke_devil: int
+    tzkal_zuk: int
+    tztok_jad: int
+    venenatis: int
+    vetion: int
+    vorkath: int
+    wintertodt: int
+    zalcano: int
+    zulrah: int
     
 
 @router.get("/v1/hiscore/", tags=["hiscore"])
@@ -173,4 +255,11 @@ async def post(hiscores: hiscore):
     '''
     insert data into database
     '''
-    pass
+    param = hiscores.dict()
+    # list of column values
+    columns = list_to_string(list(param.keys()))
+    values = list_to_string([f':{column}' for column in list(param.keys())])
+
+    sql = f'insert ignore into playerHiscoreData ({columns}) values ({values});'
+    execute_sql(sql, param)
+    return {'ok':'ok'}
