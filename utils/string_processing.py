5af92a2a71b2c62bc6a835d0bb1a072680b45a48
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-08-24 08:51:33 -0400

    Allow for special characters as the first character of RSNs

--- utils/string_processing.py
+++ utils/string_processing.py
@@ -4,4 +4,6 @@ def is_valid_rsn(rsn):
     return re.fullmatch('[\w\d _-]{1,12}', rsn)
 
 def to_jagex_name(name: str) -> str:
-    return name.replace('_', ' ').replace('-', ' ').strip()
+    #Allow for special characters as the first character of RSNs
+    jagex_name = name[0:1] + name[1:].replace('_', ' ').replace('-', ' ').strip()
+    return jagex_name
