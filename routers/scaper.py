e4fec333fcf092e1c3bc6782e4dbcf5929029728
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-09-27 15:08:36 +0200

    fix scraper

--- routers/scaper.py
+++ routers/scaper.py
@@ -2,21 +2,112 @@ import time
 from re import I
 from typing import List, Optional
 
-import Config
-import pandas as pd
-import SQL
+from sqlalchemy.sql.expression import column
+
 from database.functions import execute_sql, list_to_string, verify_token
-from fastapi import APIRouter, status
+from fastapi import APIRouter
 from pydantic import BaseModel
 
 import routers.hiscore as hi
-from routers.hiscore import hiscore
-from routers.player import Player
 
 router = APIRouter()
 
+class hiscore(BaseModel):
+    player_id: Optional[int]
+    total: int
+    Attack: int
+    Defence: int
+    Strength: int
+    Hitpoints: int
+    Ranged: int
+    Prayer: int
+    Magic: int
+    Cooking: int
+    Woodcutting: int
+    Fletching: int
+    Fishing: int
+    Firemaking: int
+    Crafting: int
+    Smithing: int
+    Mining: int
+    Herblore: int
+    Agility: int
+    Thieving: int
+    Slayer: int
+    Farming: int
+    Runecraft: int
+    Hunter: int
+    Construction: int
+    league: int
+    bounty_hunter_hunter: int
+    bounty_hunter_rogue: int
+    cs_all: int
+    cs_beginner: int
+    cs_easy: int
+    cs_medium: int
+    cs_hard: int
+    cs_elite: int
+    cs_master: int
+    lms_rank: int
+    soul_wars_zeal: int
+    abyssal_sire: int
+    alchemical_hydra: int
+    barrows_chests: int
+    bryophyta: int
+    callisto: int
+    cerberus: int
+    chambers_of_xeric: int
+    chambers_of_xeric_challenge_mode: int
+    chaos_elemental: int
+    chaos_fanatic: int
+    commander_zilyana: int
+    corporeal_beast: int
+    crazy_archaeologist: int
+    dagannoth_prime: int
+    dagannoth_rex: int
+    dagannoth_supreme: int
+    deranged_archaeologist: int
+    general_graardor: int
+    giant_mole: int
+    grotesque_guardians: int
+    hespori: int
+    kalphite_queen: int
+    king_black_dragon: int
+    kraken: int
+    kreearra: int
+    kril_tsutsaroth: int
+    mimic: int
+    nightmare: int
+    obor: int
+    sarachnis: int
+    scorpia: int
+    skotizo: int
+    tempoross:int
+    the_gauntlet: int
+    the_corrupted_gauntlet: int
+    theatre_of_blood: int
+    theatre_of_blood_hard: int
+    thermonuclear_smoke_devil: int
+    tzkal_zuk: int
+    tztok_jad: int
+    venenatis: int
+    vetion: int
+    vorkath: int
+    wintertodt: int
+    zalcano: int
+    zulrah: int
+
+class Player(BaseModel):
+    id: int
+    name: Optional[str]
+    possible_ban: Optional[bool]
+    confirmed_ban: Optional[bool]
+    confirmed_player: Optional[bool]
+    label_id: Optional[int]
+    label_jagex: Optional[int]
+
 class scraper(BaseModel):
-    hiscore: hiscore
+    hiscores: Optional[hiscore]
     player: Player
 
 async def sql_get_players_to_scrape(page=1, amount=100_000):
@@ -105,20 +196,42 @@ async def process_player(player, hiscore):
     return
 
 
-
+#TODO: rewrite
 @router.post("/scraper/hiscores/{token}", tags=["scraper"])
 async def post_hiscores_to_db(token, data: List[scraper]):
     await verify_token(token, verifcation='ban')
 
-    data = [d.__dict__ for d in data]
-    data = [
-        {
-            'player': d['player'].__dict__,
-            'hiscore': d['hiscore'].__dict__
-        } for d in data
-    ]
-
+    # get all players & all hiscores
+    data = [d.dict() for d in data]
+    players = []
+    hiscores = []
     for d in data:
-        Config.sched.add_job(process_player ,args=[d['player'], d['hiscore']], replace_existing=False, name=f'scrape_{d["player"]["name"]}')
-    del data
-    return {'OK':'OK'}
+        player_dict = d['player']
+        hiscore_dict = d['hiscores']
+
+        if not hiscore_dict:
+            players.append(player_dict)
+            continue
+
+        hiscore_dict['player_id'] = player_dict['id']
+        players.append(player_dict)
+        hiscores.append(hiscore_dict)
+    
+    # update many into players
+    values = [f'{c}=:{c}' for c in players[0].keys()]
+    values = list_to_string(values)
+    sql = f'update Players set {values} where id = :id'
+    await execute_sql(sql, players)
+    
+    # stop if there are no hiscores to insert
+    if not hiscores:
+        return
+
+    # insert many into hiscores
+    values = [f'{c}=:{c}' for c in hiscores[0].keys()]
+    values = list_to_string(values)
+    columns = list_to_string(hiscores[0].keys())
+    sql = f'insert ignore into playerHiscoreData ({columns}) values ({values})'
+    await execute_sql(sql, hiscores)
+    return
+    
