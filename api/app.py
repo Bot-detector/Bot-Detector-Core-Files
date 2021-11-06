5457c08c8783af382e1b01066b926c509ca2848b
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-11-05 21:24:53 -0400

    fixed typo

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 from api.Config import app
-from api.routers import feedback, hiscore, legacy, player, prediction, report, scaper, label
+from api.routers import feedback, hiscore, legacy, player, prediction, report, scraper, label
 
 app.include_router(hiscore.router)
 app.include_router(player.router)
@@ -7,7 +7,7 @@ app.include_router(prediction.router)
 app.include_router(feedback.router)
 app.include_router(report.router)
 app.include_router(legacy.router)
-app.include_router(scaper.router)
+app.include_router(scraper.router)
 app.include_router(label.router)
 
 
