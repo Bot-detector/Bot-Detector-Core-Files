a9e647090177cf844a47eb8b9aa7d73e04009323
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-11-11 00:15:55 -0500

    normalize new rsns

--- plugin/detect.py
+++ plugin/detect.py
@@ -14,12 +14,10 @@ from flask.json import jsonify
 detect = Blueprint('detect', __name__, template_folder='templates')
 
 def is_valid_rsn(rsn):
-    return True
-    # return re.fullmatch('[\w\d\s_-]{1,12}', rsn)
+    return re.fullmatch('[\w\d\s_-]{1,12}', rsn)
 
 def to_jagex_name(name: str) -> str:
-    #TODO Normalization on DB
-    return name
+    return name.lower().replace('_', ' ').replace('-',' ').strip()
 
 def sql_select_players(names):
     sql = "SELECT * FROM Players WHERE name in :names"
@@ -58,25 +56,29 @@ def parse_detection(data:dict) ->dict:
     }
     return param
 
-def process_data(df, clean_names, manual_detect):
+def process_data(df, names, manual_detect):
+    # 1) Ensure names are valid
+    valid_names = [name for name in names if is_valid_rsn(name)]
+
     # 2) Get IDs for all unique names
-    data = sql_select_players(clean_names)
+    data = sql_select_players(valid_names)
     
     # 3) Create entries for players that do not yet exist in Players table
     existing_names = [d.name for d in data]
-    new_names = set(clean_names).difference(existing_names)
+    new_names = set(valid_names).difference(existing_names)
     
     # 3.1) Get those players' IDs from step 3
     if new_names:
-        sql = "insert ignore into Players (name) values(:name)"
-        param = [{"name": name} for name in new_names]
+        sql = "insert ignore into Players (name, normalized_name) values (:name, :normalized_name)"
+        param = [{"name": name, "normalized_name": to_jagex_name(name)} for name in new_names]
         SQL.execute_sql(sql, param, has_return=False)
 
-        data.append(sql_select_players(new_names))
+        data.extend(sql_select_players(new_names))
 
     # 4) Insert detections into Reports table with user ids 
     # 4.1) add reported & reporter id
     df_names = pd.DataFrame(data)
+    print(df_names)
     df = df.merge(df_names, left_on="reported", right_on="name")
     
     df["reporter_id"]  = df_names.query(f"name == {df['reporter'].unique()}")['id'].to_list()[0]
@@ -116,10 +118,7 @@ def post_detect(version=None, manual_detect=0):
     names = list(df['reported'].unique())
     names.extend(df['reporter'].unique())
 
-    # 1.1) Normalize and validate all names
-    clean_names = [to_jagex_name(name) for name in names if is_valid_rsn(name)]
-    
     Config.sched.add_job(
-        process_data, args=[df, clean_names, manual_detect], name='detect' , misfire_grace_time=None, replace_existing=False
+        process_data, args=[df, names, manual_detect], name='detect' , misfire_grace_time=None, replace_existing=False
     )
     return {'OK': 'OK'}
--- utils/string_processing.py
+++ utils/string_processing.py
@@ -4,9 +4,7 @@ def is_valid_rsn(rsn):
     return re.fullmatch('[\w\d _-]{1,12}', rsn)
 
 def to_jagex_name(name: str) -> str:
-    #Allow for special characters as the first character of RSNs
-    jagex_name = name[0:1] + name[1:].replace('_', ' ').replace('-', ' ').strip()
-    return jagex_name
+    return name.lower().replace('_', ' ').replace('-',' ').strip()
 
 #https://github.com/AaronTraas/Clash-Royale-Clan-Tools/blob/79f90e5bd6d3008ebd25718f5bbdbee10c82e0ee/crtools/discord.py#L111
 #Taken from above.
