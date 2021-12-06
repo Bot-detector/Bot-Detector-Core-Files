e8893336d2c509a61cdfc78d0a5847c976db70f8
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-12-06 17:00:02 +0100

    increase default batch size

--- api/database/functions.py
+++ api/database/functions.py
@@ -147,7 +147,7 @@ async def verify_token(token:str, verifcation:str) -> bool:
 
     
 
-async def batch_function(function, data, batch_size=10):
+async def batch_function(function, data, batch_size=100):
     batches = []
     for i in range(0, len(data), batch_size):
         logger.debug(f'batch: {function.__name__}, {i}/{len(data)}')
