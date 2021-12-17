bff4a543555418c4fe42b3c046dc10836a74271f
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-12-17 15:07:30 +0100

    add is_valid_rsn & to_jagex_name

--- api/database/functions.py
+++ api/database/functions.py
@@ -1,15 +1,16 @@
 import asyncio
-from asyncio.tasks import create_task
 import logging
 import random
+import re
 import traceback
+from asyncio.tasks import create_task
 from collections import namedtuple
 
-# Although never directly used, the engines are imported to add a permanent reference 
+# Although never directly used, the engines are imported to add a permanent reference
 # to these entities to prevent the
 # garbage collector from trying to dispose of our engines.
-from api.database.database import PLAYERDATA_ENGINE, DISCORD_ENGINE
-from api.database.database import Engine, EngineType, get_session
+from api.database.database import (DISCORD_ENGINE, PLAYERDATA_ENGINE, Engine,
+                                   EngineType, get_session)
 from api.database.models import Token
 from fastapi import HTTPException
 from sqlalchemy import text
@@ -18,20 +19,21 @@ from sqlalchemy.sql.expression import select
 
 logger = logging.getLogger(__name__)
 
+
 def list_to_string(l):
     string_list = ', '.join(str(item) for item in l)
     return string_list
-    
+
+
 async def execute_sql(sql, param={}, debug=False, engine_type=EngineType.PLAYERDATA, row_count=100_000, page=1, is_retry=False, has_return=None, retry_attempt=0):
     # retry breakout
     if retry_attempt >= 5:
         logger.debug(f'To many retries')
         return None
 
-
     if not is_retry:
         has_return = True if sql.strip().lower().startswith('select') else False
-    
+
         if has_return:
             # add pagination to every query
             # max number of rows = 100k
@@ -43,7 +45,7 @@ async def execute_sql(sql, param={}, debug=False, engine_type=EngineType.PLAYERD
             # add the param
             param['offset'] = offset
             param['row_count'] = row_count
-        
+
         # parsing
         sql = text(sql)
 
@@ -55,7 +57,7 @@ async def execute_sql(sql, param={}, debug=False, engine_type=EngineType.PLAYERD
         logger.debug(f'{param=}')
 
         await engine.engine.dispose()
-    
+
     try:
         async with get_session(engine_type) as session:
             # execute session
@@ -65,24 +67,24 @@ async def execute_sql(sql, param={}, debug=False, engine_type=EngineType.PLAYERD
             # commit session
             await session.commit()
 
-
     # OperationalError = Deadlock, InternalError = lock timeout
     except OperationalError as e:
         e = e if debug else ''
         logger.debug(f'Deadlock, retrying {e}')
-        await asyncio.sleep(random.uniform(0.1,1.1))
+        await asyncio.sleep(random.uniform(0.1, 1.1))
         records = await execute_sql(sql, param, debug, engine_type, row_count, page, is_retry=True, has_return=has_return, retry_attempt=retry_attempt+1)
     except InternalError as e:
         e = e if debug else ''
         logger.debug(f'Lock, retrying: {e}')
-        await asyncio.sleep(random.uniform(0.1,1.1))
+        await asyncio.sleep(random.uniform(0.1, 1.1))
         records = await execute_sql(sql, param, debug, engine_type, row_count, page, is_retry=True, has_return=has_return, retry_attempt=retry_attempt+1)
     except Exception as e:
         logger.error('got an unkown error')
         logger.error(traceback.print_exc())
         records = None
-    
+
     return records
+
 
 class sql_cursor:
     def __init__(self, rows):
@@ -94,6 +96,7 @@ class sql_cursor:
     def rows2tuple(self):
         Record = namedtuple('Record', self.rows.keys())
         return [Record(*r) for r in self.rows.fetchall()]
+
 
 class sqlalchemy_result:
     def __init__(self, rows):
@@ -107,10 +110,11 @@ class sqlalchemy_result:
         Record = namedtuple('Record', columns)
         return [Record(*[getattr(row, col.name) for col in row.__table__.columns]) for row in self.rows]
 
-async def verify_token(token:str, verifcation:str) -> bool:
+
+async def verify_token(token: str, verifcation: str) -> bool:
     # query
     sql = select(Token)
-    sql = sql.where(Token.token==token)
+    sql = sql.where(Token.token == token)
 
     # transaction
     async with get_session(EngineType.PLAYERDATA) as session:
@@ -120,13 +124,15 @@ async def verify_token(token:str, verifcation:str) -> bool:
     data = sqlalchemy_result(data)
 
     if len(data.rows) == 0:
-        raise HTTPException(status_code=403, detail=f"insufficient permissions: {verifcation}")
+        raise HTTPException(
+            status_code=403, detail=f"insufficient permissions: {verifcation}")
 
     player_token = data.rows2tuple()
 
     # check if token exists (empty list if token does not exist)
     if not player_token:
-        raise HTTPException(status_code=403, detail=f"insufficient permissions: {verifcation}")
+        raise HTTPException(
+            status_code=403, detail=f"insufficient permissions: {verifcation}")
 
     # all possible checks
     permissions = {
@@ -138,10 +144,10 @@ async def verify_token(token:str, verifcation:str) -> bool:
 
     # get permission, default: 0
     if not permissions.get(verifcation, 0) == 1:
-        raise HTTPException(status_code=403, detail=f"insufficient permissions: {verifcation}")
+        raise HTTPException(
+            status_code=403, detail=f"insufficient permissions: {verifcation}")
     return True
 
-    
 
 async def batch_function(function, data, batch_size=100):
     batches = []
@@ -155,3 +161,11 @@ async def batch_function(function, data, batch_size=100):
     ])
 
     return
+
+
+async def is_valid_rsn(rsn: str) -> bool:
+    return re.fullmatch('[\w\d\s_-]{1,13}', rsn)
+
+
+async def to_jagex_name(name: str) -> str:
+    return name.lower().replace('_', ' ').replace('-', ' ').strip()
