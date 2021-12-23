62d910277371ae3251c24ff1de24480b8a8fa89d
y c <40169115+extreme4all@users.noreply.github.com>
2021-12-23 16:08:31 +0100

    filter for get report

--- api/routers/report.py
+++ api/routers/report.py
@@ -1,13 +1,17 @@
+
+from datetime import datetime, date
 import logging
-from typing import List
+from typing import List, Optional
 
-from sqlalchemy.sql.expression import insert
-
-from api.database.functions import execute_sql, verify_token, get_session, EngineType
-from api.database.models import Report
-from fastapi import APIRouter
+from api.database.functions import (EngineType, get_session,
+                                    verify_token, sqlalchemy_result)
+from api.database.models import Player, Report
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from sqlalchemy import update
+from sqlalchemy.sql.expression import insert, select
+from sqlalchemy import Date
+from sqlalchemy.sql import func
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
@@ -41,13 +45,42 @@ class detection(BaseModel):
     equip_ge_value: int
 
 
-@router.get("v1/report", tags=["report"])
-async def get(token: str):
+@router.get("/v1/report", tags=["report"])
+async def get(
+    token: str,
+    reportedID: Optional[int]=None,
+    reportingID: Optional[int]=None,
+    timestamp: Optional[date]=None,
+    regionID: Optional[int]=None
+    ):
     '''
     select data from database
     '''
     await verify_token(token, verifcation='hiscore')
-    pass
+
+    if None == reportedID == reportingID:
+        raise HTTPException(status_code=404, detail="reportedID or reportingID must be given")
+
+    sql = select(Report)
+
+    if not reportedID is None:
+        sql = sql.where(Report.reportedID == reportedID)
+
+    if not reportingID is None:
+        sql = sql.where(Report.reportingID == reportingID)
+
+    if not timestamp is None:
+        sql = sql.where(func.date(Report.timestamp) == timestamp)
+
+    if not regionID is None:
+        sql = sql.where(Report.region_id == regionID)
+    
+    # execute query
+    async with get_session(EngineType.PLAYERDATA) as session:
+        data = await session.execute(sql)
+
+    data = sqlalchemy_result(data)
+    return data.rows2dict()
 
 
 @router.put("/v1/report", tags=["report"])
