1342082a95cb02bd821ba1fc9059c0fee10040f4
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-01-02 19:15:36 -0500

    Revert "Release 2021 W50-52 (#292)"
    This reverts commit b94078289d7ae2a396d2e8c69d737a9163982523.

--- api/routers/player.py
+++ api/routers/player.py
@@ -1,10 +1,10 @@
 import time
 from typing import List, Optional
 
-from api.database.database import Engine, EngineType, get_session
+from api.database.database import Engine
 from api.database.functions import sqlalchemy_result, verify_token
 from api.database.models import Player as dbPlayer
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.sql.expression import insert, select, update
 
@@ -21,22 +21,24 @@ class Player(BaseModel):
     label_jagex: Optional[int]
 
 
-@router.get("/v1/player", tags=["Player"])
-async def get_player_information(
+@router.get("/v1/player", tags=["player"])
+async def get(
     token: str,
     player_name: Optional[str] = None,
-    player_id: Optional[int] = Query(None, ge=0),
-    row_count: int = Query(100_000, ge=1),
-    page: int = Query(1, ge=1)
+    player_id: Optional[int] = None,
+    label_id: Optional[int] = None,
+    row_count: int = 100_000,
+    page: int = 1
 ):
     '''
-        Select a player by name or id.
+    select data from database
     '''
-    await verify_token(token, verification='request_highscores', route='[GET]/v1/player')
+    await verify_token(token, verifcation='hiscore')
 
     # return exception if no param are given
-    if None == player_name == player_id:
-        raise HTTPException(status_code=404, detail="No valid parameters given")
+    if None == player_name == player_id == label_id:
+        raise HTTPException(
+            status_code=404, detail="No valid parameters given")
 
     # create query
     sql = select(dbPlayer)
@@ -44,77 +46,77 @@ async def get_player_information(
     # filters
     if not player_name == None:
         sql = sql.where(dbPlayer.name == player_name)
+
     if not player_id == None:
         sql = sql.where(dbPlayer.id == player_id)
+
+    if not label_id == None:
+        sql = sql.where(dbPlayer.label_id == label_id)
 
     # query pagination
     sql = sql.limit(row_count).offset(row_count*(page-1))
 
     # transaction
-    async with get_session(EngineType.PLAYERDATA) as session:
+    Session = Engine().session
+
+    async with Session() as session:
         data = await session.execute(sql)
 
     data = sqlalchemy_result(data)
     return data.rows2dict()
 
 
-@router.get("/v1/player/bulk", tags=["Player"])
-async def get_bulk_player_data_from_the_plugin_database(
+@router.post("/v1/player/bulk", tags=["player"])
+async def post_bulk(
     token: str,
-    possible_ban: Optional[int] = None,
-    confirmed_ban: Optional[int] = None,
-    confirmed_player: Optional[int] = None,
-    label_id: Optional[int] = None,
-    label_jagex: Optional[int] = None,
-    row_count: int = Query(100_000, ge=1),
-    page: int = Query(1, ge=1)
+    player_name: Optional[List[str]] = None,
+    player_id: Optional[List[int]] = None,
+    label_id: Optional[List[int]] = None,
+    row_count: int = 100_000,
+    page: int = 1
     ):
     '''
-        Selects bulk player data from the plugin database.
+        select data from database
     '''
-    await verify_token(token, verification='request_highscores', route='[POST]/v1/player')
+    await verify_token(token, verifcation='hiscore')
 
     # return exception if no param are given
-    if None == possible_ban == confirmed_ban == confirmed_player == label_id == label_jagex:
-        raise HTTPException(status_code=404, detail="No param given")
+    if None == player_name == player_id == label_id:
+        raise HTTPException(
+            status_code=404, detail="No valid parameters given")
 
     # create query
     sql = select(dbPlayer)
 
-    # filters    
     # filters
-    if not possible_ban is None:
-        sql = sql.where(dbPlayer.possible_ban == possible_ban)
+    if not player_name == None:
+        sql = sql.where(dbPlayer.name.in_(player_name))
 
-    if not confirmed_ban is None:
-        sql = sql.where(dbPlayer.confirmed_ban == confirmed_ban)
-
-    if not confirmed_player is None:
-        sql = sql.where(dbPlayer.confirmed_player == confirmed_player)
-
-    if not label_id is None:
-        sql = sql.where(dbPlayer.label_id == label_id)
-
-    if not label_jagex is None:
-        sql = sql.where(dbPlayer.label_jagex == label_jagex)
+    if not player_id == None:
+        sql = sql.where(dbPlayer.id.in_(player_id))
+    
+    if not label_id == None:
+        sql = sql.where(dbPlayer.label_id.in_(label_id))
 
     # query pagination
     sql = sql.limit(row_count).offset(row_count*(page-1))
 
     # transaction
-    async with get_session(EngineType.PLAYERDATA) as session:
+    Session = Engine().session
+
+    async with Session() as session:
         data = await session.execute(sql)
 
     data = sqlalchemy_result(data)
     return data.rows2dict()
 
 
-@router.put("/v1/player", tags=["Player"])
-async def update_existing_player_data(player: Player, token: str):
+@router.put("/v1/player", tags=["player"])
+async def put(player: Player, token: str):
     '''
-        Update player & return updated player.
+    update data into database
     '''
-    await verify_token(token, verification='verify_ban')
+    await verify_token(token, verifcation='ban')
 
     # param
     param = player.dict()
@@ -131,7 +133,9 @@ async def update_existing_player_data(player: Player, token: str):
     sql_select = sql_select.where(dbPlayer.id == player_id)
 
     # transaction
-    async with get_session(EngineType.PLAYERDATA) as session:
+    Session = Engine().session
+
+    async with Session() as session:
         await session.execute(sql_update)
         await session.commit()
         data = await session.execute(sql_select)
@@ -140,12 +144,12 @@ async def update_existing_player_data(player: Player, token: str):
     return data.rows2dict()
 
 
-@router.post("/v1/player", tags=["Player"])
-async def insert_new_player_data_into_plugin_database(player_name: str, token: str):
+@router.post("/v1/player", tags=["player"])
+async def post(player_name: str, token: str):
     '''
-        Insert new player & return player.
+    insert data into database
     '''
-    await verify_token(token, verification='verify_ban', route='[POST]/v1/player')
+    await verify_token(token, verifcation='ban')
 
     sql_insert = insert(dbPlayer)
     sql_insert = sql_insert.values(name=player_name)
@@ -154,7 +158,9 @@ async def insert_new_player_data_into_plugin_database(player_name: str, token: s
     sql_select = select(dbPlayer)
     sql_select = sql_select.where(dbPlayer.name == player_name)
 
-    async with get_session(EngineType.PLAYERDATA) as session:
+    Session = Engine().session
+
+    async with Session() as session:
         await session.execute(sql_insert)
         await session.commit()
         data = await session.execute(sql_select)
--- api/routers/report.py
+++ api/routers/report.py
@@ -1,18 +1,9 @@
+from typing import List
 
-from datetime import date
-import logging
-from typing import List, Optional
-
-from api.database.functions import (EngineType, get_session,
-                                    verify_token, sqlalchemy_result)
-from api.database.models import Player, Prediction, Report, ReportLatest
-from fastapi import APIRouter, HTTPException, Query
+from api.database.functions import execute_sql, verify_token
+from fastapi import APIRouter
 from pydantic import BaseModel
-from sqlalchemy import update
-from sqlalchemy.sql.expression import insert, select
-from sqlalchemy.sql import func
-
-
+import logging
 logger = logging.getLogger(__name__)
 router = APIRouter()
 
@@ -45,178 +36,49 @@ class detection(BaseModel):
     equip_ge_value: int
 
 
-@router.get("/v1/report", tags=["Report"])
-async def get_reports_from_plugin_database(
-    token: str,
-    reportedID: Optional[int]= Query(None, ge=0),
-    reportingID: Optional[int]= Query(None, ge=0),
-    timestamp: Optional[date]= None,
-    regionID: Optional[int]= Query(None, ge=0, le=100000)
-    ):
+@router.get("v1/report", tags=["report"])
+async def get(token: str):
     '''
-        Select report data.
+    select data from database
     '''
-    await verify_token(token, verification='verify_ban', route='[GET]/v1/report/')
-
-    if None == reportedID == reportingID:
-        raise HTTPException(status_code=404, detail="reportedID or reportingID must be given")
-
-    sql = select(Report)
-
-    if not reportedID is None:
-        sql = sql.where(Report.reportedID == reportedID)
-
-    if not reportingID is None:
-        sql = sql.where(Report.reportingID == reportingID)
-
-    if not timestamp is None:
-        sql = sql.where(func.date(Report.timestamp) == timestamp)
-
-    if not regionID is None:
-        sql = sql.where(Report.region_id == regionID)
-    
-    # execute query
-    async with get_session(EngineType.PLAYERDATA) as session:
-        data = await session.execute(sql)
-
-    data = sqlalchemy_result(data)
-    return data.rows2dict()
-
-
-@router.put("/v1/report", tags=["Report"])
-async def update_reports(old_user_id: int, new_user_id: int, token: str):
-    '''
-        Update the reports from one reporting user to another.
-    '''
-    await verify_token(token, verification='verify_ban', route='[PUT]/v1/report/')
-    # can be used for name change
-
-    sql = update(Report)
-    sql = sql.values(Report.reportingID == new_user_id)
-    sql = sql.where(Report.reportingID == old_user_id)
-
-    async with get_session(EngineType.PLAYERDATA) as session:
-        await session.execute(sql)
-
-    return {'OK': 'OK'}
-
-
-@router.post("/v1/report", tags=["Report"])
-async def insert_report(token: str, detections: List[detection]):
-    '''
-        Work in progress 
-        Insert report.
-    '''
-    await verify_token(token, verification='verify_ban', route='[POST]/v1/report/')
-
-    sql = insert(Report)
+    await verify_token(token, verifcation='hiscore')
     pass
 
-@router.get("/v1/report/prediction", tags=["Report", "Business"])
-async def get_report_by_prediction(
-    token: str,
-    label_jagex: int,
-    predicted_confidence: int,
-    prediction: Optional[str]=None,
-    real_player: Optional[int]=None,
-    crafting_bot: Optional[int]=None,
-    timestamp: Optional[date]=None,
-    region_id: Optional[int]=None
-    ):
+
+@router.put("v1/report", tags=["report"])
+async def put(old_user_id: int, new_user_id: int, token: str):
     '''
-        Gets account based upon the prediction features.
-        Business service: Twitter
+    update data into database
     '''
-    await verify_token(token, verification='verify_ban', route='[GET]/v1/report/prediction')
-    
-    
-    sql = select(   
-        Player.id, 
-        Prediction.Prediction, 
-        Prediction.Predicted_confidence
-    ).distinct()
+    await verify_token(token, verifcation='ban')
+    # can be used for name change
+    sql = ('''
+    UPDATE Reports
+    SET
+        reportingID = :NewUser
+    where 
+        reportingID = :OldUser
 
-    sql = sql.where(Prediction.Predicted_confidence >= predicted_confidence)
-    sql = sql.where(Player.label_jagex == label_jagex)
+    ''')
+    param = {}
+    param['NewUser'] = new_user_id
+    param['OldUser'] = old_user_id
 
-    if not prediction is None:
-        sql = sql.where(Prediction.Prediction == prediction)
+    await execute_sql(sql, param)
+    return {'OK':'OK'}
 
-    if not real_player is None:
-        sql = sql.where(Prediction.Real_Player < real_player)
 
-    if not crafting_bot is None:
-        sql = sql.where(Prediction.Crafting_bot > crafting_bot)
-
-    if not timestamp is None:
-        sql = sql.where(func.date(Report.timestamp) == timestamp)
-    
-    if not region_id is None:
-        sql = sql.where(Report.region_id == region_id)
-    
-    sql = sql.join(Report, Player.id == Report.reportedID)
-    sql = sql.join(Prediction, Player.id == Prediction.id)
-
-    # execute query
-    async with get_session(EngineType.PLAYERDATA) as session:
-        data = await session.execute(sql)
-
-    output = []
-    for row in data:
-        mydata = {}
-        mydata['id'] = row[0]
-        mydata['prediction'] = row[1]
-        mydata['Predicted_confidence'] = row[2]
-        output.append(mydata)
-
-    return output
-
-@router.get("/v1/report/latest", tags=["Report"])
-async def get_latest_report_of_a_user(
-    token: str,
-    reported_id: int = Query(..., ge=0)
-    ):
-    
+@router.post("v1/report", tags=["report"])
+async def post(token:str,detections: List[detection]):
     '''
-        Select the latest report data, by reported user
+    insert data into database
     '''
-    
-    await verify_token(token, verification='verify_ban', route='[GET]/v1/report/latest')
+    await verify_token(token, verifcation='ban')
+    sql = ('''
+        insert ignore into Reports
+    ''')
 
-    sql = select(ReportLatest)
-
-    if not reported_id is None:
-        sql = sql.where(ReportLatest.reported_id == reported_id)
-    
-    # execute query
-    async with get_session(EngineType.PLAYERDATA) as session:
-        data = await session.execute(sql)
-
-    data = sqlalchemy_result(data)
-    return data.rows2dict()
-
-@router.get("/v1/report/latest/bulk", tags=["Report"])
-async def get_bulk_latest_report_data(
-    token: str,
-    region_id: Optional[int] = Query(None, ge=0, le=25000),
-    timestamp: Optional[date] = None
-    ):
-    '''
-        get the player count in bulk by region and or date
-    '''
-    await verify_token(token, verification='verify_ban', route='[GET]/v1/report/latest/bulk')
-
-    sql = select(ReportLatest)
-
-    if not timestamp is None:
-        sql = sql.where(func.date(ReportLatest.timestamp) == timestamp)
-
-    if not region_id is None:
-        sql = sql.where(ReportLatest.region_id == region_id)
-
-    # execute query
-    async with get_session(EngineType.PLAYERDATA) as session:
-        data = await session.execute(sql)
-
-    data = sqlalchemy_result(data)
-    return data.rows2dict()
+    if len(detections) > 5000:
+        return {'OK':'OK'}
+        
+    pass
