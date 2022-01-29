7ed44f09785c18c92d6340877a6e7b2a4d0ea9f9
y c <40169115+extreme4all@users.noreply.github.com>
2022-01-29 18:39:28 +0100

    create contributor

--- api/routers/report.py
+++ api/routers/report.py
@@ -1,19 +1,16 @@
-
 import logging
 import time
 from datetime import date
 from typing import List, Optional
 
 import pandas as pd
-from pydantic.fields import Field
-from api.database.functions import (EngineType, batch_function, get_session,
-                                    jagexify_names_list, sqlalchemy_result,
-                                    to_jagex_name, verify_token)
-from api.database.models import (Player, Prediction, Report, ReportLatest,
-                                 stgReport)
-from fastapi import APIRouter, HTTPException, Query, Request, status
+from api.database import functions
+from api.database.models import Player, Prediction, Report, ReportLatest, stgReport
+from fastapi import APIRouter, HTTPException, Query, status
 from pydantic import BaseModel
+from pydantic.fields import Field
 from sqlalchemy import update
+from sqlalchemy.orm import aliased
 from sqlalchemy.sql import func
 from sqlalchemy.sql.expression import insert, select
 
@@ -31,55 +28,57 @@ upper_gear_cost = 1_000_000_000_000
 
 async def sql_select_players(names: List[str]) -> List:
     sql = select(Player)
-    sql = sql.where(Player.normalized_name.in_(tuple(await jagexify_names_list(names))))
-    async with get_session(EngineType.PLAYERDATA) as session:
+    sql = sql.where(
+        Player.normalized_name.in_(tuple(await functions.jagexify_names_list(names)))
+    )
+    async with functions.get_session(functions.EngineType.PLAYERDATA) as session:
         data = await session.execute(sql)
-    data = sqlalchemy_result(data)
+    data = functions.sqlalchemy_result(data)
     return [] if not data else data.rows2dict()
 
 
 async def sql_insert_player(new_names: List[dict]) -> None:
     sql = insert(Player)
-    async with get_session(EngineType.PLAYERDATA) as session:
+    async with functions.get_session(functions.EngineType.PLAYERDATA) as session:
         await session.execute(sql, new_names)
         await session.commit()
 
 
 async def sql_insert_report(param: dict) -> None:
     sql = insert(stgReport)
-    async with get_session(EngineType.PLAYERDATA) as session:
+    async with functions.get_session(functions.EngineType.PLAYERDATA) as session:
         await session.execute(sql, param)
         await session.commit()
 
 
 async def parse_detection(data: dict) -> dict:
-    gmt = time.gmtime(data['ts'])
-    human_time = time.strftime('%Y-%m-%d %H:%M:%S', gmt)
+    gmt = time.gmtime(data["ts"])
+    human_time = time.strftime("%Y-%m-%d %H:%M:%S", gmt)
 
-    equipment = data.get('equipment', {})
+    equipment = data.get("equipment", {})
 
     param = {
-        'reportedID': data.get('id'),
-        'reportingID': data.get('reporter_id'),
-        'region_id': data.get('region_id'),
-        'x_coord': data.get('x_coord'),
-        'y_coord': data.get('y_coord'),
-        'z_coord': data.get('z_coord'),
-        'timestamp': human_time,
-        'manual_detect': data.get('manual_detect'),
-        'on_members_world': data.get('on_members_world'),
-        'on_pvp_world': data.get('on_pvp_world'),
-        'world_number': data.get('world_number'),
-        'equip_head_id': equipment.get('equip_head_id'),
-        'equip_amulet_id': equipment.get('equip_amulet_id'),
-        'equip_torso_id': equipment.get('equip_torso_id'),
-        'equip_legs_id': equipment.get('equip_legs_id'),
-        'equip_boots_id': equipment.get('equip_boots_id'),
-        'equip_cape_id': equipment.get('equip_cape_id'),
-        'equip_hands_id': equipment.get('equip_hands_id'),
-        'equip_weapon_id': equipment.get('equip_weapon_id'),
-        'equip_shield_id': equipment.get('equip_shield_id'),
-        'equip_ge_value': data.get('equip_ge_value', 0)
+        "reportedID": data.get("id"),
+        "reportingID": data.get("reporter_id"),
+        "region_id": data.get("region_id"),
+        "x_coord": data.get("x_coord"),
+        "y_coord": data.get("y_coord"),
+        "z_coord": data.get("z_coord"),
+        "timestamp": human_time,
+        "manual_detect": data.get("manual_detect"),
+        "on_members_world": data.get("on_members_world"),
+        "on_pvp_world": data.get("on_pvp_world"),
+        "world_number": data.get("world_number"),
+        "equip_head_id": equipment.get("equip_head_id"),
+        "equip_amulet_id": equipment.get("equip_amulet_id"),
+        "equip_torso_id": equipment.get("equip_torso_id"),
+        "equip_legs_id": equipment.get("equip_legs_id"),
+        "equip_boots_id": equipment.get("equip_boots_id"),
+        "equip_cape_id": equipment.get("equip_cape_id"),
+        "equip_hands_id": equipment.get("equip_hands_id"),
+        "equip_weapon_id": equipment.get("equip_weapon_id"),
+        "equip_shield_id": equipment.get("equip_shield_id"),
+        "equip_ge_value": data.get("equip_ge_value", 0),
     }
     return param
 
@@ -118,16 +117,19 @@ async def get_reports_from_plugin_database(
     reportedID: Optional[int] = Query(None, ge=0),
     reportingID: Optional[int] = Query(None, ge=0),
     timestamp: Optional[date] = None,
-    regionID: Optional[int] = Query(None, ge=0, le=100000)
+    regionID: Optional[int] = Query(None, ge=0, le=100000),
 ):
-    '''
-        Select report data.
-    '''
-    await verify_token(token, verification='verify_ban', route='[GET]/v1/report/')
+    """
+    Select report data.
+    """
+    await functions.verify_token(
+        token, verification="verify_ban", route="[GET]/v1/report/"
+    )
 
     if None == reportedID == reportingID:
         raise HTTPException(
-            status_code=404, detail="reportedID or reportingID must be given")
+            status_code=404, detail="reportedID or reportingID must be given"
+        )
 
     sql = select(Report)
 
@@ -144,29 +146,31 @@ async def get_reports_from_plugin_database(
         sql = sql.where(Report.region_id == regionID)
 
     # execute query
-    async with get_session(EngineType.PLAYERDATA) as session:
+    async with functions.get_session(functions.EngineType.PLAYERDATA) as session:
         data = await session.execute(sql)
 
-    data = sqlalchemy_result(data)
+    data = functions.sqlalchemy_result(data)
     return data.rows2dict()
 
 
 @router.put("/v1/report", tags=["Report"])
 async def update_reports(old_user_id: int, new_user_id: int, token: str):
-    '''
-        Update the reports from one reporting user to another.
-    '''
-    await verify_token(token, verification='verify_ban', route='[PUT]/v1/report/')
+    """
+    Update the reports from one reporting user to another.
+    """
+    await functions.verify_token(
+        token, verification="verify_ban", route="[PUT]/v1/report/"
+    )
     # can be used for name change
 
     sql = update(Report)
     sql = sql.values(Report.reportingID == new_user_id)
     sql = sql.where(Report.reportingID == old_user_id)
 
-    async with get_session(EngineType.PLAYERDATA) as session:
+    async with functions.get_session(functions.EngineType.PLAYERDATA) as session:
         await session.execute(sql)
 
-    return {'OK': 'OK'}
+    return {"OK": "OK"}
 
 
 @router.post("/v1/report", status_code=status.HTTP_201_CREATED, tags=["Report"])
@@ -174,16 +178,13 @@ async def insert_report(
     detections: List[detection],
     manual_detect: int = Query(0, ge=0, le=1),
 ):
-    '''
-        Inserts detections into to the plugin database.
-    '''
+    """
+    Inserts detections into to the plugin database.
+    """
 
     # remove duplicates
     df = pd.DataFrame([d.dict() for d in detections])
-    df.drop_duplicates(
-        subset=['reporter', 'reported', 'region_id'],
-        inplace=True
-    )
+    df.drop_duplicates(subset=["reporter", "reported", "region_id"], inplace=True)
 
     sender = list(df["reporter"].unique())
 
@@ -192,44 +193,46 @@ async def insert_report(
         logger.debug({"message": "Too many reports", "sender": sender})
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Your sightings are out of bounds. Contact plugin support on our Discord."
+            detail=f"Your sightings are out of bounds. Contact plugin support on our Discord.",
         )
 
-    if len(sender[0]) > 12 and not sender[0] == 'AnonymousUser':
+    if len(sender[0]) > 12 and not sender[0] == "AnonymousUser":
         logger.debug({"message": "invalid username", "sender": sender})
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail=f"Invalid username. Contact plugin support on our Discord."
+            detail=f"Invalid username. Contact plugin support on our Discord.",
         )
 
     # data validation, checks for correct timing
     now = int(time.time())
-    lower_bound = (now - back_time_buffer)
-    mask = (df['ts'] > now)
-    mask = mask | (df['ts'] < lower_bound)
+    lower_bound = now - back_time_buffer
+    mask = df["ts"] > now
+    mask = mask | (df["ts"] < lower_bound)
 
     if len(df[~mask]) == 0:
-        logger.debug({
-            "message": "Data contains out of bounds time",
-            "reporter": df["reporter"].unique(),
-            "time": df[mask].values[0]
-        })
+        logger.debug(
+            {
+                "message": "Data contains out of bounds time",
+                "reporter": df["reporter"].unique(),
+                "time": df[mask].values[0],
+            }
+        )
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Your sightings contain out of bounds time. Contact plugin support on our Discord."
+            detail=f"Your sightings contain out of bounds time. Contact plugin support on our Discord.",
         )
 
     df = df[~mask]
 
     # Successful query
-    logger.debug({"message":f"Received: {len(df)} from: {sender}"})
+    logger.debug({"message": f"Received: {len(df)} from: {sender}"})
 
     # 1) Get a list of unqiue reported names and reporter name
-    names = list(df['reported'].unique())
-    names.extend(df['reporter'].unique())
+    names = list(df["reported"].unique())
+    names.extend(df["reporter"].unique())
 
     # 1.1) Normalize and validate all names
-    clean_names = await jagexify_names_list(names)
+    clean_names = await functions.jagexify_names_list(names)
 
     # 2) Get IDs for all unique names
     data = await sql_select_players(clean_names)
@@ -242,13 +245,13 @@ async def insert_report(
     if new_names:
         param = [{"name": name, "normalized_name": name} for name in new_names]
 
-        await batch_function(sql_insert_player, param)
+        await functions.batch_function(sql_insert_player, param)
         data.extend(await sql_select_players(new_names))
 
     if len(data) == 0:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Your sightings are incomplete. Contact plugin support on our Discord."
+            detail="Your sightings are incomplete. Contact plugin support on our Discord.",
         )
 
     # 4) Insert detections into Reports table with user ids
@@ -257,24 +260,24 @@ async def insert_report(
 
     df = df.merge(df_names, left_on="reported", right_on="name")
 
-    reporter = [await to_jagex_name(n) for n in df['reporter'].unique()]
-    reporter = df_names.query(f"normalized_name == {reporter}")['id'].to_list()
+    reporter = [await functions.to_jagex_name(n) for n in df["reporter"].unique()]
+    reporter = df_names.query(f"normalized_name == {reporter}")["id"].to_list()
 
     if len(reporter) == 0:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="There was an error processing your name. Contact plugin support on our Discord."
+            detail="There was an error processing your name. Contact plugin support on our Discord.",
         )
 
     df["reporter_id"] = reporter[0]
-    df['manual_detect'] = manual_detect
+    df["manual_detect"] = manual_detect
 
     # 4.2) parse data to param
-    data = df.to_dict('records')
+    data = df.to_dict("records")
     param = [await parse_detection(d) for d in data]
 
     # 4.3) parse query
-    await batch_function(sql_insert_report, param)
+    await functions.batch_function(sql_insert_report, param)
     return {"detail": "ok"}
 
 
@@ -287,18 +290,18 @@ async def get_report_by_prediction(
     real_player: Optional[int] = None,
     crafting_bot: Optional[int] = None,
     timestamp: Optional[date] = None,
-    region_id: Optional[int] = None
+    region_id: Optional[int] = None,
 ):
-    '''
-        Gets account based upon the prediction features.
-        Business service: Twitter
-    '''
-    await verify_token(token, verification='verify_ban', route='[GET]/v1/report/prediction')
+    """
+    Gets account based upon the prediction features.
+    Business service: Twitter
+    """
+    await functions.verify_token(
+        token, verification="verify_ban", route="[GET]/v1/report/prediction"
+    )
 
     sql = select(
-        Player.id,
-        Prediction.Prediction,
-        Prediction.Predicted_confidence
+        Player.id, Prediction.Prediction, Prediction.Predicted_confidence
     ).distinct()
 
     sql = sql.where(Prediction.Predicted_confidence >= predicted_confidence)
@@ -323,31 +326,30 @@ async def get_report_by_prediction(
     sql = sql.join(Prediction, Player.id == Prediction.id)
 
     # execute query
-    async with get_session(EngineType.PLAYERDATA) as session:
+    async with functions.get_session(functions.EngineType.PLAYERDATA) as session:
         data = await session.execute(sql)
 
     output = []
     for row in data:
         mydata = {}
-        mydata['id'] = row[0]
-        mydata['prediction'] = row[1]
-        mydata['Predicted_confidence'] = row[2]
+        mydata["id"] = row[0]
+        mydata["prediction"] = row[1]
+        mydata["Predicted_confidence"] = row[2]
         output.append(mydata)
 
     return output
 
 
 @router.get("/v1/report/latest", tags=["Report"])
-async def get_latest_report_of_a_user(
-    token: str,
-    reported_id: int = Query(..., ge=0)
-):
+async def get_latest_report_of_a_user(token: str, reported_id: int = Query(..., ge=0)):
 
-    '''
-        Select the latest report data, by reported user
-    '''
+    """
+    Select the latest report data, by reported user
+    """
 
-    await verify_token(token, verification='verify_ban', route='[GET]/v1/report/latest')
+    await functions.verify_token(
+        token, verification="verify_ban", route="[GET]/v1/report/latest"
+    )
 
     sql = select(ReportLatest)
 
@@ -355,10 +357,10 @@ async def get_latest_report_of_a_user(
         sql = sql.where(ReportLatest.reported_id == reported_id)
 
     # execute query
-    async with get_session(EngineType.PLAYERDATA) as session:
+    async with functions.get_session(functions.EngineType.PLAYERDATA) as session:
         data = await session.execute(sql)
 
-    data = sqlalchemy_result(data)
+    data = functions.sqlalchemy_result(data)
     return data.rows2dict()
 
 
@@ -366,12 +368,14 @@ async def get_latest_report_of_a_user(
 async def get_bulk_latest_report_data(
     token: str,
     region_id: Optional[int] = Query(None, ge=0, le=25000),
-    timestamp: Optional[date] = None
+    timestamp: Optional[date] = None,
 ):
-    '''
-        get the player count in bulk by region and or date
-    '''
-    await verify_token(token, verification='verify_ban', route='[GET]/v1/report/latest/bulk')
+    """
+    get the player count in bulk by region and or date
+    """
+    await functions.verify_token(
+        token, verification="verify_ban", route="[GET]/v1/report/latest/bulk"
+    )
 
     sql = select(ReportLatest)
 
@@ -382,8 +386,66 @@ async def get_bulk_latest_report_data(
         sql = sql.where(ReportLatest.region_id == region_id)
 
     # execute query
-    async with get_session(EngineType.PLAYERDATA) as session:
+    async with functions.get_session(functions.EngineType.PLAYERDATA) as session:
         data = await session.execute(sql)
 
-    data = sqlalchemy_result(data)
+    data = functions.sqlalchemy_result(data)
     return data.rows2dict()
+
+
+@router.get(
+    "/v1/report/count", status_code=status.HTTP_200_OK, tags=["Report", "Business"]
+)
+async def get_contributions(
+    user_name: str = Query(..., min_length=1, max_length=12),
+):
+    """
+    Allows for a player to see their contributions.
+    """
+
+    if not await functions.is_valid_rsn(user_name):
+        logger.debug(f"Bad Name passed for v1/report/count  | {user_name=}")
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Your name could not be processed. Contact plugin support on our Discord.",
+        )
+
+    user_name = await functions.to_jagex_name(user_name)
+
+    pl = aliased(Player, name="pl")
+    ban = aliased(Player, name="ban")
+
+    sql = select(
+        func.ifnull(Report.manual_detect, 0),
+        ban.confirmed_ban,
+        ban.possible_ban,
+        ban.confirmed_player,
+        func.count(Report.reportedID.distinct()),
+    )
+
+    sql = sql.where(pl.normalized_name == user_name)
+    sql = sql.group_by(
+        Report.manual_detect, ban.confirmed_ban, ban.possible_ban, ban.confirmed_player
+    )
+
+    sql = sql.join(pl, pl.id == Report.reportingID)
+    sql = sql.join(ban, ban.id == Report.reportedID)
+
+    fields = [
+        "manual_detect",
+        "confirmed_ban",
+        "possible_ban",
+        "confirmed_player",
+        "count",
+    ]
+    async with functions.get_session(functions.EngineType.PLAYERDATA) as session:
+        data = await session.execute(sql)
+    data = [{k: v for k, v in zip(fields, d)} for d in data]
+
+    if len(data) == 0:
+        logger.debug(f"No Data found for {user_name=}.")
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"No Data found for {user_name}. Contact Plugin Support on our Discord.",
+        )
+    return data
