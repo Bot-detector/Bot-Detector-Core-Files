513c5545a2f0b85ed2c2be88aba6df76652b0308
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-08-16 21:33:34 -0400

    Add support for censoring logs

--- api/utils/logging_helpers.py
+++ api/utils/logging_helpers.py
@@ -1,4 +1,16 @@
 from fastapi import Request
+from typing import List
+from functools import reduce
 
-def build_route_log_string(request: Request) -> str:
-  return f"[{request.method}] Path: {request.url.path} Query Params: {request.query_params}"
+
+def build_route_log_string(request: Request, censored_strings: List[str]) -> str:
+  log_str = f"[{request.method}] Path: {request.url.path} Query Params: {request.query_params}"
+
+  return log_str if not censored_strings else censor_log_entry(log_str, censored_strings)
+
+
+def censor_log_entry(log_str: str, censored_strings: List[str]) -> str:
+
+  [log_str := log_str.replace(censored, "***") for censored in censored_strings]
+
+  return log_str
