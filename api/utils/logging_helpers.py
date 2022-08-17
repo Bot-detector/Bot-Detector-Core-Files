94deda4f9fd29f149f25c775d945041b8aacc21d
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-08-16 21:47:21 -0400

    Add docstring

--- api/utils/logging_helpers.py
+++ api/utils/logging_helpers.py
@@ -3,8 +3,28 @@ from typing import List
 from functools import reduce
 
 
-def build_route_log_string(request: Request, censored_strings: List[str]) -> str:
+def build_route_log_string(request: Request, censored_strings: List[str] = []) -> str:
+  """
+  Creates a string for logging purposes that contains information about how a route was accessed.
+
+  Parameters
+  ----------
+  request : Request
+    A Starlette Request object containing information about the HTTP request received.
+  
+  censored_strings : List[str] (Optional)
+    A list of substrings that will be replaced with '***' in the log string.
+
+  Returns
+  ----------
+    A string containing information about the request.
+
+  Example:
+    "[POST] Path: /1.3.2/site/discord_user/*** Query Params: test=true"
+  """
   log_str = f"[{request.method}] Path: {request.url.path} Query Params: {request.query_params}"
+
+  print(log_str)
 
   return log_str if not censored_strings else censor_log_entry(log_str, censored_strings)
 
