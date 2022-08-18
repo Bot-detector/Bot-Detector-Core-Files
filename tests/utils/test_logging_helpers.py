9e6720cf53d6adbe94c4bb224d4d4ac0d0e5c372
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-08-17 21:41:05 -0400

    Add test_censor_log_entry test

--- tests/utils/test_logging_helpers.py
+++ tests/utils/test_logging_helpers.py
@@ -5,8 +5,8 @@ from unittest.mock import patch
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from api.Config import token
-from api.database.functions import verify_token
 from api.routers import legacy
+from api.utils import logging_helpers
 
 
 request_path = f"/discord/get_linked_accounts/{token}/1"
@@ -21,8 +21,21 @@ def test_build_route_log_string(test_client):
     with patch.object(legacy, 'verify_token', return_value=None) as mock:
         test_client.get(request_path)
 
-        mock.assert_called_once_with(token, "verify_players", request_path.replace(token, "***"))
+        expected_log_str = '[GET] Path: /discord/get_linked_accounts/***/1 Query Params: '
+
+        mock.assert_called_once_with(token, verification="verify_players", route=expected_log_str)
 
 
-def censor_log_entry():
-    ...
+def test_censor_log_entry():
+    """
+    Tests if censor_log_entry replaces the specified substrings with '***'
+    """
+
+    censored_str = logging_helpers.censor_log_entry(request_path, [token, "discord"])
+
+    expected_str = "/***/get_linked_accounts/***/1"
+
+    assert token not in censored_str
+    assert "discord" not in censored_str
+
+    assert censored_str == expected_str
