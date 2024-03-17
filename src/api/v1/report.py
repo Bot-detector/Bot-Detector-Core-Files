0d823ef870837e436383c7e3172581df32324bf3
extreme4all <40169115+extreme4all@users.noreply.github.com>
2024-03-17 17:36:15 +0100

    found new names

--- src/api/v1/report.py
+++ src/api/v1/report.py
@@ -281,7 +281,7 @@ async def insert_report(
     # Create entries for players that do not yet exist in Players table
     existing_names = [d["normalized_name"] for d in data]
     new_names = set([name for name in valid_names]).difference(existing_names)
-
+    logger.debug(f"Found new names: {len(new_names)}")
     # Get new player id's
     if new_names:
         param = [
